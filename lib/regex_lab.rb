ba13467d64c8c0d2e156a1014ffdc43bd5584ff8
Dane Olsen <dno1995@gmail.com>
2019-11-27 02:15:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,24 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiou]|[AEIOU]/) != nil
+    then true
+  else false
+  end
+  # word =~ /^[aeiou]/
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un[a-z]*ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b[a-zA-Z]{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\A[A-Z]\w*[.,;:?!]\z/) != nil
+    then true
+  else false
+  end
 end
 
 def valid_phone_number?(phone)
